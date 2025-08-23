42ee6e508c45ca83c2bc17c57aa26ce4efe61c6f
shilinnull <shilinnull@163.com>
2025-08-23 08:37:31 +0000

    Util done

--- Test/Http/http.hpp
+++ Test/Http/http.hpp
@@ -5,7 +5,148 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include "../../source/server.hpp"
+
+#define DEFALT_TIMEOUT 10
+
+std::unordered_map<int, std::string> _statu_msg = {
+    {100, "Continue"},
+    {101, "Switching Protocol"},
+    {102, "Processing"},
+    {103, "Early Hints"},
+    {200, "OK"},
+    {201, "Created"},
+    {202, "Accepted"},
+    {203, "Non-Authoritative Information"},
+    {204, "No Content"},
+    {205, "Reset Content"},
+    {206, "Partial Content"},
+    {207, "Multi-Status"},
+    {208, "Already Reported"},
+    {226, "IM Used"},
+    {300, "Multiple Choice"},
+    {301, "Moved Permanently"},
+    {302, "Found"},
+    {303, "See Other"},
+    {304, "Not Modified"},
+    {305, "Use Proxy"},
+    {306, "unused"},
+    {307, "Temporary Redirect"},
+    {308, "Permanent Redirect"},
+    {400, "Bad Request"},
+    {401, "Unauthorized"},
+    {402, "Payment Required"},
+    {403, "Forbidden"},
+    {404, "Not Found"},
+    {405, "Method Not Allowed"},
+    {406, "Not Acceptable"},
+    {407, "Proxy Authentication Required"},
+    {408, "Request Timeout"},
+    {409, "Conflict"},
+    {410, "Gone"},
+    {411, "Length Required"},
+    {412, "Precondition Failed"},
+    {413, "Payload Too Large"},
+    {414, "URI Too Long"},
+    {415, "Unsupported Media Type"},
+    {416, "Range Not Satisfiable"},
+    {417, "Expectation Failed"},
+    {418, "I'm a teapot"},
+    {421, "Misdirected Request"},
+    {422, "Unprocessable Entity"},
+    {423, "Locked"},
+    {424, "Failed Dependency"},
+    {425, "Too Early"},
+    {426, "Upgrade Required"},
+    {428, "Precondition Required"},
+    {429, "Too Many Requests"},
+    {431, "Request Header Fields Too Large"},
+    {451, "Unavailable For Legal Reasons"},
+    {501, "Not Implemented"},
+    {502, "Bad Gateway"},
+    {503, "Service Unavailable"},
+    {504, "Gateway Timeout"},
+    {505, "HTTP Version Not Supported"},
+    {506, "Variant Also Negotiates"},
+    {507, "Insufficient Storage"},
+    {508, "Loop Detected"},
+    {510, "Not Extended"},
+    {511, "Network Authentication Required"}};
+
+std::unordered_map<std::string, std::string> _mime_msg = {
+    {".aac", "audio/aac"},
+    {".abw", "application/x-abiword"},
+    {".arc", "application/x-freearc"},
+    {".avi", "video/x-msvideo"},
+    {".azw", "application/vnd.amazon.ebook"},
+    {".bin", "application/octet-stream"},
+    {".bmp", "image/bmp"},
+    {".bz", "application/x-bzip"},
+    {".bz2", "application/x-bzip2"},
+    {".csh", "application/x-csh"},
+    {".css", "text/css"},
+    {".csv", "text/csv"},
+    {".doc", "application/msword"},
+    {".docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"},
+    {".eot", "application/vnd.ms-fontobject"},
+    {".epub", "application/epub+zip"},
+    {".gif", "image/gif"},
+    {".htm", "text/html"},
+    {".html", "text/html"},
+    {".ico", "image/vnd.microsoft.icon"},
+    {".ics", "text/calendar"},
+    {".jar", "application/java-archive"},
+    {".jpeg", "image/jpeg"},
+    {".jpg", "image/jpeg"},
+    {".js", "text/javascript"},
+    {".json", "application/json"},
+    {".jsonld", "application/ld+json"},
+    {".mid", "audio/midi"},
+    {".midi", "audio/x-midi"},
+    {".mjs", "text/javascript"},
+    {".mp3", "audio/mpeg"},
+    {".mpeg", "video/mpeg"},
+    {".mpkg", "application/vnd.apple.installer+xml"},
+    {".odp", "application/vnd.oasis.opendocument.presentation"},
+    {".ods", "application/vnd.oasis.opendocument.spreadsheet"},
+    {".odt", "application/vnd.oasis.opendocument.text"},
+    {".oga", "audio/ogg"},
+    {".ogv", "video/ogg"},
+    {".ogx", "application/ogg"},
+    {".otf", "font/otf"},
+    {".png", "image/png"},
+    {".pdf", "application/pdf"},
+    {".ppt", "application/vnd.ms-powerpoint"},
+    {".pptx", "application/vnd.openxmlformats-officedocument.presentationml.presentation"},
+    {".rar", "application/x-rar-compressed"},
+    {".rtf", "application/rtf"},
+    {".sh", "application/x-sh"},
+    {".svg", "image/svg+xml"},
+    {".swf", "application/x-shockwave-flash"},
+    {".tar", "application/x-tar"},
+    {".tif", "image/tiff"},
+    {".tiff", "image/tiff"},
+    {".ttf", "font/ttf"},
+    {".txt", "text/plain"},
+    {".vsd", "application/vnd.visio"},
+    {".wav", "audio/wav"},
+    {".weba", "audio/webm"},
+    {".webm", "video/webm"},
+    {".webp", "image/webp"},
+    {".woff", "font/woff"},
+    {".woff2", "font/woff2"},
+    {".xhtml", "application/xhtml+xml"},
+    {".xls", "application/vnd.ms-excel"},
+    {".xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+    {".xml", "application/xml"},
+    {".xul", "application/vnd.mozilla.xul+xml"},
+    {".zip", "application/zip"},
+    {".3gp", "video/3gpp"},
+    {".3g2", "video/3gpp2"},
+    {".7z", "application/x-7z-compressed"}};
 
 class Util
 {
@@ -53,6 +194,146 @@ public:
             return false;
         }
         ifs.close();
+        return true;
+    }
+
+    static bool WriteFile(const std::string &filename, std::string &buf)
+    {
+        std::ofstream ofs(filename, std::ios::binary | std::ios::trunc);
+        if (!ofs.is_open())
+        {
+            LOG(LogLevel::WARNING) << "open file failed! filename: " << filename;
+            return false;
+        }
+        ofs.write(buf.c_str(), buf.size());
+        if (ofs.good() == false)
+        {
+            LOG(LogLevel::WARNING) << "write file failed! filename: " << filename;
+            ofs.close();
+            return false;
+        }
+        ofs.close();
+        return true;
+    }
+
+    // URL编码，避免URL中资源路径与查询字符串中的特殊字符与HTTP请求中特殊字符产生歧义
+    static std::string UrlEncode(const std::string &url, bool convert_space_to_plus)
+    {
+        // RFC3986文档规定，编码格式 %HH
+        std::string res;
+        for (auto &c : url)
+        {
+            if (c == '.' || c == '-' || c == '_' || c == '~' || isalnum(c))
+            {
+                res += c;
+                continue;
+            }
+            if (c == ' ' && convert_space_to_plus) // W3C标准中规定查询字符串中的空格，需要编码为+， 解码则是+转空格
+            {
+                res += '+';
+                continue;
+            }
+            // 剩下的字符都是需要编码成为 %HH 格式
+            char tmp[4] = {0};
+            snprintf(tmp, 4, "%%%02X", c);
+            res += tmp;
+        }
+    }
+
+    static char HEXTOI(char c)
+    {
+        if (c >= '0' && c <= '9')
+            return c - '0';
+        else if (c >= 'a' && c <= 'z')
+            return c - 'a' + 10;
+        else if (c >= 'A' && c <= 'Z')
+            return c - 'A' + 10;
+        return -1;
+    }
+
+    static std::string UrlDecode(const std::string &url, bool convert_space_to_plus)
+    {
+        std::string res;
+        for (int i = 0; i < url.size(); i++)
+        {
+            if (url[i] == '+' && convert_space_to_plus)
+            {
+                res += ' ';
+                continue;
+            }
+
+            if (i + 2 < url.size() && url[i] == '%')
+            {
+                char v1 = HEXTOI(url[i + 1]);
+                char v2 = HEXTOI(url[i + 2]);
+                char v = (v1 << 4) + v2;
+                res += v;
+                i += 2;
+                continue;
+            }
+            res += url[i];
+        }
+        return res;
+    }
+
+    // 响应状态码的描述信息获取
+    static std::string StatuDesc(int statu)
+    {
+        auto it = _statu_msg.find(statu);
+        if (it == _statu_msg.end())
+            return "Unknow";
+        return it->second;
+    }
+    // 根据文件后缀名获取文件mime
+    static std::string ExtMine(const std::string &filename)
+    {
+        // 先获取文件扩展名
+        size_t pos = filename.find_last_of('.');
+        if (pos == std::string::npos)
+        {
+            return "application/octet-stream";
+        }
+        // 获取mime
+        std::string ext = filename.substr(pos);
+        auto it = _mime_msg.find(ext);
+        if (it == _mime_msg.end())
+        {
+            return "application/octet-stream";
+        }
+        return it->second;
+    }
+
+    // 判断一个文件是否是一个目录
+    static bool IsDirectory(const std::string &filename)
+    {
+        struct stat st;
+        int ret = stat(filename.c_str(), &st);
+        if (ret < 0)
+        {
+            return false;
+        }
+        return S_ISDIR(st.st_mode);
+    }
+
+    // http请求的资源路径有效性判断
+    static bool ValidPath(const std::string &path)
+    {
+        // 思想：按照/进行路径分割，根据有多少子目录，计算目录深度，有多少层，深度不能小于0
+        std::vector<std::string> subdir;
+        Split(path, "/", &subdir);
+
+        int level = 0;
+        for (auto &dir : subdir)
+        {
+            if (dir == "..")
+            {
+                level--;
+                if (level < 0)
+                    return false;
+                continue;
+            }
+            level++;
+        }
         return true;
     }
 };
