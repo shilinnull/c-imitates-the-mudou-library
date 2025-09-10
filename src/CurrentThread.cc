a34f886091ed17f1742c15b36f0480dc5c0f0961
shilinnull <shilinnull@163.com>
2025-09-10 10:11:25 +0000

    EventLoop

---
+++ src/CurrentThread.cc
@@ -0,0 +1,12 @@
+#include "CurrentThread.h"
+
+namespace CurrentThread
+{
+    __thread int t_cachedTid = 0;
+
+    void cacheTid()
+    {
+        t_cachedTid = static_cast<pid_t>(::syscall(SYS_gettid));
+    }
+
+} // namespace CurrentThread
